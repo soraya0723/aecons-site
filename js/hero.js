d2fd375647c36fd2ec460428c1cde97c927bae88
Sorraya Jaafar <aeconssite@gmail.com>
2025-08-15 18:42:42 +0800

    Your changes

--- js/hero.js
+++ js/hero.js
@@ -1,7 +1,7 @@
 if (document.querySelector('.hero-swiper')) {
   new Swiper('.hero-swiper', {
     loop: true,
-    autoplay: { delay: 4000, disableOnInteraction: false },
+    autoplay: { delay: 8000, disableOnInteraction: false }, // matches zoom-out duration
     effect: 'fade',
     fadeEffect: { crossFade: true }
   });
